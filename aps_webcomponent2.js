c6ccbae3bb43af60facb581d5d728edb7222718c
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-03-01 14:17:11 +0000

    new widget

---
+++ aps_webcomponent2.js
@@ -0,0 +1,49 @@
+(function() {
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<form id="form">
+			<fieldset>
+				<legend>Custom Widget Text</legend>
+				<table>
+					<tr>
+						<td>Inicial Value:</td>
+						<td><input id="aps_valInicial" type="string"></td>
+                        <td>Final Value:</td>
+						<td><input id="aps_valFinal" type="string"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+	`;
+
+	class HelloWorldAps extends HTMLElement {
+		constructor() {
+			super();
+			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
+			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+		}
+
+		_submit(e) {
+			e.preventDefault();
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							widgetText: this.widgetText
+						}
+					}
+			}));
+		}
+
+		set widgetValues(valInicial, valFinal) {
+			this._shadowRoot.getElementById("aps_valInicial").value = valInicial;
+            this._shadowRoot.getElementById("aps_valFinal").value = valFinal;
+		}
+
+		get widgetValues() {
+			return this._shadowRoot.getElementById("aps_valInicial").value, this._shadowRoot.getElementById("aps_valFinal").value;
+		}
+	}
+
+customElements.define("com-sap-sample-helloworld1-aps", HelloWorldAps);
+})();
---
+++ webcomponent2.js
@@ -0,0 +1,97 @@
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+        <h1 id="progressTotal">20</h1>
+        <div class="progressionBar">
+            <div id="Bar"></div>      
+        </div>
+        <style>
+        .progressionBar{
+            width: 200px;
+            height: 20px;
+        }
+        #Bar{
+            width: 0px;
+            height: 20px;
+        }
+        </style>
+    `;
+
+    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
+            this._tagContainer;
+            this._tagIncreaseText = "h1";
+            this._tagTextValue = '';
+            this._tagProgressionBar = "<div class='progressionBar'><div id='Bar'></div></div>";
+            this._tagBar = document.getElementById('Bar').style.width;
+            
+        }
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+                this.redraw();
+            }
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
+
+        get widgetText() {
+			return this._tagIncreaseText;
+		}
+
+        set widgetText(valInicial, valFinal) {
+            if (isset(valInicial) && isset(valFinal)) {
+                valInicial = parseInt(valInicial);
+                valFinal =  parseInt(valFinal);
+
+                let progressionGrowth = ((valFinal - valInicial) / valInicial) * 100;
+                let barWidth = (200 * progressionGrowth) / 100;
+                if (barWidth > 200) {
+                    document.getElementById('Bar').style.width = '200px';
+                } else {
+                    document.getElementById('Bar').style.width = barWidth + 'px';
+                }
+                this._tagTextValue = 'There was an increase in ' + progressionGrowth + '%';
+            }
+		}
+
+
+        redraw(){
+                if (this._tagContainer){
+                    this._tagContainer.parentNode.removeChild(this._tagContainer);
+                }
+        
+                var shadow = window.getSelection(this._shadowRoot);
+                this._tagContainer = document.createElement(this._tagIncreaseText);
+                var theText = document.createTextNode(this._tagTextValue);    
+                this._tagContainer.appendChild(theText); 
+                this._shadowRoot.appendChild(this._tagContainer);
+                this._shadowRoot.appendChild(this._tagProgressionBar);
+            }
+    });
+})();
