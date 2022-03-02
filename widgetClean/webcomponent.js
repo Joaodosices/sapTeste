5e1a514abf94a0f4095528a7ae1f66033b8911a2
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-03-02 14:39:23 +0000

    added code

--- widgetClean/webcomponent.js
+++ widgetClean/webcomponent.js
@@ -26,12 +26,16 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-            // this._tagContainer;
-            // this._tagIncreaseText = "h1";
+            
+            this._inicialValue;
+            this._finalValue;
+            this._tagTextValue = '';
         }
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -46,10 +50,46 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+                this.redraw();
+            }
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
+        }
+
+        set widgetInicialValue(valInicial) {
+			this._inicialValue = valInicial;
+		}
+        get widgetInicialValue() {
+			return this._inicialValue
+		}
+
+        set widgetFinalValue(valFinal) {
+            this._finalValue = valFinal;
+		}
+        get widgetFinalValue() {
+            return this._finalValue
+		}
+
+        set widgetIncrease(val) {
+            let progressionGrowth = (val / this._inicialValue) * 100;
+            this._barWidth = (200 * progressionGrowth) / 100; 
+            this._tagTextValue = 'There was an increase in ' + val + '%';
+		}
+        get widgetIncrease() {
+			return this._tagTextValue;
+		}
+        
+        redraw(){
+            document.getElementById('progressTotal').innerText = this._tagTextValue;
+
+            if (this._barWidth > 200) {
+                document.getElementById('Bar').style.width = '200px';
+            } else {
+                document.getElementById('Bar').style.width = barWidth + 'px';
+            }
         }
     };
     customElements.define('com-sap-sample-clean', HelloWorld2);
