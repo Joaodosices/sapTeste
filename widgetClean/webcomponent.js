d95d1c074ce4232a3c8b993a48e5f22d4796eb66
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-03-02 15:26:54 +0000

    s

--- widgetClean/webcomponent.js
+++ widgetClean/webcomponent.js
@@ -26,7 +26,8 @@
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-            
+            this._tagContainer;
+            this._tagIncreaseText = "h1";
             this._inicialValue;
             this._finalValue;
             this._tagTextValue = '';
@@ -80,18 +81,27 @@
             let progressionGrowth = (number / this._inicialValue) * 100;
             this._barWidth = (200 * progressionGrowth) / 100; 
             this._tagTextValue = 'There was an increase in ' + number + '%';
+            this.redraw();
 		}
         get widgetIncrease() {
 			return this._tagTextValue;
 		}
 
         redraw(){
-            document.getElementById('progressTotal').innerText = this._tagTextValue;
-
-            if (this._barWidth > 200) {
-                document.getElementById('Bar').style.width = '200px';
-            } else {
-                document.getElementById('Bar').style.width = barWidth + 'px';
+            if (this._tagTextValue != null) {
+                // if (this._tagContainer){
+                //     this._tagContainer.parentNode.removeChild(this._tagContainer);
+                // }
+                // this._tagContainer = document.createElement(this._tagIncreaseText);
+                // var theText = document.createTextNode(this._tagTextValue);    
+                // this._tagContainer.appendChild(theText); 
+                document.getElementById('progressTotal').innerText = this._tagTextValue;
+    
+                if (this._barWidth > 200) {
+                    document.getElementById('Bar').style.width = '200px';
+                } else {
+                    document.getElementById('Bar').style.width = barWidth + 'px';
+                }
             }
         }
     };
