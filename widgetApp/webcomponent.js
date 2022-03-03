cf0daf64fcb9643d5c8b461a54b64c8fe153c2fb
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-03-03 14:19:13 +0000

    sa

--- widgetApp/webcomponent.js
+++ widgetApp/webcomponent.js
@@ -10,6 +10,7 @@
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._textOutput = '';
         }
 
         //Fired when the widget is added to the html DOM of the page
@@ -28,12 +29,21 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
+            this._shadowRoot.getElementsByTagName('h1').innerHTML = oChangedProperties['textOutput'];
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
         }
 
+        // //Getters and Setters
+        // get textOutput() {
+        //     return this._text;
+        // }
+
+        // set textOutput(value) {
+        //     this._textOutput = value;
+        // }
 
     };
     customElements.define('com-sap-sample-widgetapp', WidgetApp);
