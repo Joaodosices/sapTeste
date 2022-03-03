45eab645784936f7d5c353330e284897b5b97d77
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-03-03 14:00:19 +0000

    New widget

---
+++ widgetApp/webcomponent.js
@@ -0,0 +1,40 @@
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+        
+    `;
+
+    class WidgetApp extends HTMLElement {
+
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+        }
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
+
+
+    };
+    customElements.define('com-sap-sample-widgetapp', WidgetApp);
+})();
