435a27c4e86829ee3087167073ce3044cb267033
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-03-03 14:03:14 +0000

    as

--- widgetApp/webcomponent.js
+++ widgetApp/webcomponent.js
@@ -1,7 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        
+        <h1>Hello World</h1>
     `;
 
     class WidgetApp extends HTMLElement {
