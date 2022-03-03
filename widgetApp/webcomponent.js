e6b73749c7f7ccdf797fbc002337524866bf3d0b
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-03-03 16:38:30 +0000

    as

--- widgetApp/webcomponent.js
+++ widgetApp/webcomponent.js
@@ -1,9 +1,9 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1 id="val1">1</h1>
+        <h1 id="val1">0</h1>
         <h1 id="val2">0</h1>
-        <h1 id="val3"> </h1>
+        <h1 id="val3">Final</h1>
     `;
 
     class WidgetApp extends HTMLElement {
@@ -52,11 +52,10 @@
             let valInicial = parseInt(this._valInicial);
             let valFinal = parseInt(this._valFinal);
 
-            let sum = valFinal - valFinal;
+            let sum = valFinal - valInicial;
             let growth = (sum / valInicial) * 100;
-            console.log(growth);
 
-            return "There was an increase in " + sum
+            return `There was an increase in ${growth}%`
         }
 
         //Getters and Setters
