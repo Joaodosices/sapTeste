167b3afcbd9588bcb27ea2855278cc50c140ce1e
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-05-09 16:04:28 +0100

    c

--- filterTableCustom/webcomponent.js
+++ filterTableCustom/webcomponent.js
@@ -1,7 +1,26 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <div id="root"></div>
+        <div id="ui5_content" name="ui5_content">
+            <slot name="content"></slot>
+        </div>
+
+        <script id="Oview" name ="Oview" type="sapui5/xmlview">
+            <mvc:View
+                controllerName="myView.Template"
+                xmlns:l="sap.ui.layout"
+                xmlns:mvc="sap.ui.core.mvc"
+                xmlns="sap.m">
+
+                <l:VerticalLayout
+                    class="sapUiContentPadding"
+                    width="100%">
+                    <l:content>
+                        <Input id="passwordInput" type="Password placeholder="Teste input" liveChange="onButtonPress" />
+                    </l:content>
+                <l:VerticalLayout>
+            ></mvc:View>
+        </script>
     `;
 
     class FilterTable extends HTMLElement {
@@ -10,6 +29,10 @@
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+
+            _id = createGuid();
+            this._shadowRoot.querySelector("#oView").id = _id + "_oView";
+
             this.firstConnection = false;
             this._listDimensions = [];
             this._cleanListDimensions = [];
@@ -37,7 +60,7 @@
                 console.log(this._cleanListDimensions); 
                 this.clearListMembers();
                 console.log(this._cleanListMembers);
-                console.log("DONE X2");
+                console.log("DONE X3");
                 this.draw();
             }
         }
@@ -82,10 +105,44 @@
             }
         }
         draw(){
-            // this._cleanListDimensions.join(")(");
-            // console.log(Table_1.getDataSource().getMembers(this._cleanListDimensions[0][0]));
             this._shadowRoot.getElementById("root").innerHTML = "<h1>Hi!</h1>";
         }
     };
     customElements.define('com-sap-sample-filtertable', FilterTable);
 })();
+
+function loadthis(that){
+    var that_ = that;
+
+    let content = document.createElement("div");
+    content.slot = "content";
+    that_.appendChild(content);
+
+    sap.ui.getCore().attachInit(function() {
+        "use strict";
+
+        // CONTROLLER
+        sap.ui.define([
+            "jquery.sap.global",
+            "sap/ui/core/mvc/Controller"
+        ], function(jQuery, Controller){
+            "use strict";
+
+            return Controller.extend("myView.Template", {
+                onButtonPress: function(oEvent){
+                    _password = oView.byId("passwordInput").getValue();
+                    that._firePropertiesChanged();
+                    console.log(_password);
+                }
+            });
+        });
+        var oView = sap.ui.xmlview({
+            viewContent: jQuery(_shadowRoot.getElementById(_id + "_oView")).html(),
+        });
+        oView.placeAt(content);
+
+        if (that_._designMode) {
+            oView.byId("passwordInput").setEnabled(false);
+        }
+    });
+}
