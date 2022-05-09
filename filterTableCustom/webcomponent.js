4905fbb3d3c079eade515d7c1b4c1a7681147995
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-05-09 17:30:41 +0100

    x

--- filterTableCustom/webcomponent.js
+++ filterTableCustom/webcomponent.js
@@ -39,11 +39,12 @@
             this._listMembers = [];
             this._cleanListMembers = [];
         }
+        
         createGuid(){
-            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-            );
+            //Using UUID for now
+            return self.crypto.randomUUID();
         }
+
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
             this.firstConnection = true; 
