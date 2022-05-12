87268e85c197814729f37a03b7743bc5541a9e81
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-05-12 18:01:10 +0100

    z

--- ui5webComponentsTest/webcomponent.js
+++ ui5webComponentsTest/webcomponent.js
@@ -7,7 +7,7 @@
         <ui5-option icon="laptop" selected>Desktop</ui5-option>
     </ui5-select>
 
-    <script src="ui5webComponentsTest/uiComponents.js"></script>
+    <script src="https://joaodosices.github.io/sapTeste/ui5webComponentsTest/uiComponents.js"></script>
     `;
 
     class WebComponentTest extends HTMLElement {
