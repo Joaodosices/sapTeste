1f5e0c03835a9191e63b02276a1f58509764b02a
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-05-27 08:11:09 +0100

    x

--- filterTableCustom/aps_webcomponent.js
+++ filterTableCustom/aps_webcomponent.js
@@ -6,7 +6,7 @@
 				<table>
 					<tr>
 						<td>Vertical Layout:</td>
-						<td><input id="aps_VerticalLayout" type="radio"></td>
+						<td><input id="aps_VerticalLayout" type="radio" checked="checked"></td>
 						<td>Horizontal Layout:</td>
 						<td><input id="aps_HorizontalLayout" type="radio"></td>
 					</tr>
@@ -20,21 +20,15 @@
 			super();
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this._shadowRoot.getElementById("aps_VerticalLayout").addEventListener("click", this._submitLayout.bind(this));
-			this._shadowRoot.getElementById("aps_HorizontalLayout").addEventListener("click", this._submitLayout.bind(this));
+			this._shadowRoot.getElementById("aps_VerticalLayout").addEventListener("submit", this._submitVerticalLayout.bind(this));
+			this._shadowRoot.getElementById("aps_HorizontalLayout").addEventListener("submit", this._submitHorizontalLayout.bind(this));
 			this._option = "";
-
 		}
 
-		_submitLayout(e) {
+		_submitVerticalLayout(e) {
 			e.preventDefault();
 			if (this._shadowRoot.getElementById("aps_VerticalLayout").checked === true) {
 				this._option = "VerticalLayout";
-				this._shadowRoot.getElementById("aps_HorizontalLayout").checked = false;
-			}
-			if (this._shadowRoot.getElementById("aps_HorizontalLayout").checked === true) {
-				this._option = "HorizontalLayout";
-				this._shadowRoot.getElementById("aps_VerticalLayout").checked = false;
 			}
 			console.log(this._option);
 			this.dispatchEvent(new CustomEvent("propertiesChanged", {
@@ -44,9 +38,26 @@
 						}
 					}
 			}));
-			// this.setLayoutOrientation(this._option);
+			this._shadowRoot.getElementById("aps_VerticalLayout").checked;
+			this._shadowRoot.getElementById("aps_HorizontalLayout").checked = false;
 		}
 
+		_submitHorizontalLayout(e) {
+			e.preventDefault();
+			if (this._shadowRoot.getElementById("aps_HorizontalLayout").checked === true) {
+				this._option = "HorizontalLayout";
+			}
+			console.log(this._option);
+			this.dispatchEvent(new CustomEvent("propertiesChanged", {
+					detail: {
+						properties: {
+							orientationStyle: this._option
+						}
+					}
+			}));
+			this._shadowRoot.getElementById("aps_VerticalLayout").checked = false;
+			this._shadowRoot.getElementById("aps_HorizontalLayout").checked;
+		}
 		set orientationStyle(val) {
 			this._option = val;
 		}
@@ -54,20 +65,6 @@
 			return this._option;
 		}
 
-        // setLayoutOrientation(newDimensionToClear){
-        //     this._option = newDimensionToClear;
-        //     // fire "properties changed"
-        //     this.dispatchEvent(new CustomEvent("propertiesChanged", {
-        //     detail: {
-        //         properties: {
-        //             orientationStyle: this._option
-        //         }
-        //     }
-        //     }));
-        // }
-		// getLayoutOrientation(){
-        //     return this._option
-        // }
 	}
 
 customElements.define("com-sap-sample-filtertable-aps", ApsWebComponent);
