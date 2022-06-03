b5796165e68d4dfb08b010ab684f5dfe2f23ddfe
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-06-03 21:43:19 +0100

    x

--- navigationList/webcomponent.js
+++ navigationList/webcomponent.js
@@ -67,6 +67,7 @@
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
             if (this._firstConnection){
+                console.log("After update")
                 loadthis(this);
             }
         }
