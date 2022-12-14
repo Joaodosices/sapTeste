9d0bb9119ef869dde7414dba22439d952bf2cc61
Joao Fonseca <joaofrancofonseca@gmail.com>
2022-12-14 17:20:46 +0000

    x

---
+++ menuHomePage/webcomponent.js
@@ -0,0 +1,44 @@
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+    <h1>Hello World</h1>
+    `;
+
+    class HomePage extends HTMLElement {
+
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            
+        }
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+            this.firstConnection = true; 
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        }
+            
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this.firstConnection === true){
+                
+            }
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
+
+        //
+
+    };
+    customElements.define('com-sap-sample-homepage', HomePage);
+})();
