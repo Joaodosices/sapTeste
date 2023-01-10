0ac42e7044c19ee05eb1501d8bb4b7933199a5c5
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-01-10 15:39:56 +0000

    x

--- menuHomePage/webcomponent.js
+++ menuHomePage/webcomponent.js
@@ -6,7 +6,7 @@
         #root{
             width: 700px;
             display: grid;
-            grid-template-columns: 260px 260px;
+            grid-template-columns: 255px 255px;
             background: transparent;
         }
         .areaOption{
