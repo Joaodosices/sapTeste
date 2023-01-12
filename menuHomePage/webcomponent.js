563857050dd783678b2fe8ad2eafc1fb1aaa5409
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-01-12 14:44:12 +0000

    x

--- menuHomePage/webcomponent.js
+++ menuHomePage/webcomponent.js
@@ -318,7 +318,13 @@ function events(optionSelected, that, arrOptionsList, arrOptionsLinks, arrMenuOp
                                 <p>` + element2 + `</p>
                             </div> 
                         </a>`
-                   
+                        
+                        if (newOptionsList.length === 0) {
+                            _that._shadowRoot.getElementById("root").style.gridTemplateColumns = "230px 230px"
+                        }
+                        if (newOptionsList.length >= 1) {
+                            _that._shadowRoot.getElementById("root").style.gridTemplateColumns = "230px 230px 230px"
+                        }
 
                         // optionsText = optionsText + `
                         // <div class='areaOption'> 
