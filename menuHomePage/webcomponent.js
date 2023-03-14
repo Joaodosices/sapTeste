c59fa083df28398fdc90c879dd879d40645f38d1
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-03-14 00:20:11 +0000

    x

--- menuHomePage/webcomponent.js
+++ menuHomePage/webcomponent.js
@@ -527,11 +527,10 @@ function events(optionSelected, that, arrOptionsList, arrOptionsLinks, arrMenuOp
             _that._shadowRoot.querySelectorAll(`.secondMenuArea`)[0].style.visibility = "visible"
             for (let x = 0; x < _that._shadowRoot.querySelectorAll(`.secondMenuArea`).length; x++) {
                 _that._shadowRoot.querySelectorAll(`.secondMenuArea`)[x].style.visibility = "visible"
+                _that._shadowRoot.querySelectorAll(`.secondMenuArea`)[x].getElementsByClassName(`front`)[0].style.left = "0%"
+                _that._shadowRoot.querySelectorAll(`.secondMenuArea`)[x].getElementsByClassName(`back`)[0].style.left = "100%"
             }
-            if (optionSelected.length !== 0){
-                _that._shadowRoot.querySelectorAll(`.front`)[optionSelected].style.left = "0%"
-                _that._shadowRoot.querySelectorAll(`.back`)[optionSelected].style.left = "100%"
-            }
+            
         }
     })
 }
