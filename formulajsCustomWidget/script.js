0c40be761a87127c0e23153ba94f8786e46dba24
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-05-10 14:01:24 +0100

    x

--- formulajsCustomWidget/script.js
+++ formulajsCustomWidget/script.js
@@ -1,13 +1,11 @@
 // import * as formulajs from '/@formulajs/formulajs' // import entire package
     function makeFormula(typeFormula, arg1, arg2, arg3){
         let formula = "formulajs." + typeFormula + "(" + arg1 + ", " + arg2 + ", " + arg3 + ")"
-        // formulajs.formula(arg1, arg2, arg3)
-        console.log(formula)
-        console.log(eval(formula))
-        // return formulajs.call(formula, arg1, arg2, arg3)
-        // formulajs.DATE()
+        
+        return eval(formula)
     }
 
-    makeFormula("DATE", 2008, 7, 8)
+    let data = makeFormula("DATE", 2008, 7, 8)
+    console.log(data)
 // console.log(formulajs.DATE(2008, 7, 8))
 // console.log(formulajs.SUM([1, 2, 3]))
