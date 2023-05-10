4f9ef819e0054fbae4c3c6f7d3de736475d6ccde
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-05-10 13:08:52 +0100

    x

--- formulajsCustomWidget/script.js
+++ formulajsCustomWidget/script.js
@@ -1,4 +1,10 @@
 // import * as formulajs from '/@formulajs/formulajs' // import entire package
+    function makeFormula(typeFormula, arg1, arg2, arg3){
+        let formula = typeFormula
+        
+        return formulajs.formula(arg1, arg2, arg3)
+    }
 
-console.log(formulajs.DATE(2008, 7, 8))
-console.log(formulajs.SUM([1, 2, 3]))
+    makeFormula("DATE", 2008, 7, 8)
+// console.log(formulajs.DATE(2008, 7, 8))
+// console.log(formulajs.SUM([1, 2, 3]))
