b31a0fa7221521e5eb7a196b91ca3b34cda4badd
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-05-10 14:14:44 +0100

    x

--- formulajsCustomWidget/script.js
+++ formulajsCustomWidget/script.js
@@ -1,7 +1,16 @@
 // import * as formulajs from '/@formulajs/formulajs' // import entire package
-    function makeFormula(typeFormula, arg1, arg2, arg3){
-        let formula = "formulajs." + typeFormula + "(" + arg1 + ", " + arg2 + ", " + arg3 + ")"
-        
+    function makeFormula(typeFormula, arg1, arg2, arg3, arg4, arg5, arg6, arg7){
+        ListArguments = [arg1, arg2, arg3, arg4, arg5, arg6, arg7]
+        let formula = "formulajs." + typeFormula + "("
+        for (let x = 0; x < ListArguments.length; x++) {
+            const element = ListArguments[x];
+            if (element != null) {
+                formula = formula + element + ", "
+            }
+        }
+        formula = formula + ")"
+        // + arg1 + ", " + arg2 + ", " + arg3 + ")"
+        console.log(formula)
         return eval(formula)
     }
 
