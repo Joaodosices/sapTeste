0ed2c30624b023cdb2fe6666bdae6ed63ca72d68
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-05-11 12:34:21 +0100

    x

--- formulajsCustomWidget/webcomponent.js
+++ formulajsCustomWidget/webcomponent.js
@@ -41,6 +41,10 @@
             loadLibrary(libraryUrl, function() {
                 // The library has been loaded, you can now use its functionality
                 // Your code here...
+                console.log("TESTEEEE")
+                console.log(this._functionN)
+                console.log(this._functionN.length)
+        
                 if (this._functionN.length > 0) {
                     let formula = makeFormula(this._functionN, this._param1, this._param2, this._param3, this._param4, this._param5, this._param6, this._param7)
                   
