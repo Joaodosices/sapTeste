672e8e5276e3d0473ba11be4c36da78115af472c
Joao Fonseca <joaofrancofonseca@gmail.com>
2023-05-12 10:56:37 +0100

    x

--- formulajsCustomWidget/webcomponent.js
+++ formulajsCustomWidget/webcomponent.js
@@ -46,7 +46,7 @@
                 if (this._functionN != "-") {
                     let formula = makeFormula(this._functionN, this._param1, this._param2, this._param3, this._param4, this._param5, this._param6, this._param7)
                   
-                    // console.log(formula)
+                    console.log(formula)
                     this._result = formula
                     // console.log("this._result:" + this._result)
                     this.setResult(this._result)
